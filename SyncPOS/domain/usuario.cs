bf72f26447b794f84bcd3c2d3991f858dd6d2f65
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-08-27 15:04:26 -0500

    se agrego la clase usuario

---
+++ SyncPOS/domain/usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace SyncPOS.domain
+{
+    [DataContract]
+    public class usuario
+    {
+        [DataMember]
+        public string user_name { set; get; }
+
+        [DataMember]
+        public string password { set; get; }
+
+        [DataMember]
+        public string tipo_usuario { set; get; }
+
+        [DataMember]
+        public bool enable { set; get; }
+
+        [DataMember]
+        public DateTime fecha_registro { set; get; }
+    }
+}
