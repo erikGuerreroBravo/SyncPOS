8e99ef21d1b67f087d864901a965945e7cb0d75c
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-08-27 14:59:03 -0500

    se agrego la clase inventario_fisico

---
+++ SyncPOS/domain/inventario_fisico.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace SyncPOS.domain
+{
+    [DataContract]
+    public class inventario_fisico
+    {
+
+        [DataMember]
+        public Guid id_inventario_fisico { set; get; }
+
+        [DataMember]
+        public Guid id_proveedor { set; get; }
+
+        [DataMember]
+        public string user_name { set; get; }
+
+        [DataMember]
+        public DateTime fecha_ini { set; get; }
+
+        [DataMember]
+        public string fecha_fin { set; get; }
+
+        [DataMember]
+        public DateTime fecha_registro { set; get; }
+    }
+}
