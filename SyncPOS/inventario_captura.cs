fa0aa670d0c887a82a879f6c014f46ef5e6c7914
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-08-27 16:40:03 -0500

    se agrego modificaciones para inventario_captura

--- SyncPOS/inventario_captura.cs
+++ SyncPOS/inventario_captura.cs
@@ -1,11 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.ComponentModel;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
 
 namespace SyncPOS
 {
-    public class inventario_captura
+    [Table(Name = "dbo.inventario_captura")]
+    public class inventario_captura : INotifyPropertyChanging, INotifyPropertyChanged
     {
+        #region Atributos
+        private static PropertyChangingEventArgs emptyChangingEventArgs = new PropertyChangingEventArgs(string.Empty);
+        private Guid _id_captura;
+        private Guid _id_inventario_fisico;
+        private long _num_captura;
+        private string _cod_barras;
+        private DateTime _fecha_captura;
+        private Decimal _cant_cja;
+        private Decimal _cant_pza;
+        private EntityRef<articulo> _articulo;
+        private EntityRef<inventario_fisico> _inventario_fisico;
+        #endregion
     }
 }
