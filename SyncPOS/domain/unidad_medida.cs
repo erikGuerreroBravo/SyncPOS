ba1e9b7b133ae353cd7542e6dbb223be65c0d6bb
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-08-27 15:03:20 -0500

    se agrego la clase unidad_medida

---
+++ SyncPOS/domain/unidad_medida.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace SyncPOS.domain
+{
+    [DataContract]
+    public class unidad_medida
+    {
+        [DataMember]
+        public Guid id_unidad { set; get; }
+
+        [DataMember]
+        public string descripcion { set; get; }
+
+        [DataMember]
+        public DateTime fecha_registro { set; get; }
+    }
+}
