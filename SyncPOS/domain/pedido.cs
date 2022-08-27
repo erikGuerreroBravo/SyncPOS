48d5dfde30f2765ac270abf4ca080c7cdb964fd0
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-08-27 15:00:18 -0500

    se agrego pedido

---
+++ SyncPOS/domain/pedido.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace SyncPOS.domain
+{
+    [DataContract]
+    public class pedido
+    {
+        [DataMember]
+        public Guid id_pedido { set; get; }
+
+        [DataMember]
+        public long num_pedido { set; get; }
+
+        [DataMember]
+        public DateTime fecha_pedido { set; get; }
+
+        [DataMember]
+        public Guid id_proveedor { set; get; }
+
+        [DataMember]
+        public string status_pedido { set; get; }
+
+        [DataMember]
+        public DateTime fecha_registro { set; get; }
+    }
+}
