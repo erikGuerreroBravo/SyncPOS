6b09e3f43406e48251fe4c98649f1a86d56dd395
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-08-27 15:09:07 -0500

    se agrego la calse de CtrlException

---
+++ SyncPOS/domain/usuario_permiso.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace SyncPOS.domain
+{
+    [DataContract]
+    public class usuario_permiso
+    {
+        [DataMember]
+        public string user_name { set; get; }
+
+        [DataMember]
+        public string id_permiso { set; get; }
+
+        [DataMember]
+        public Decimal valor_num { set; get; }
+
+        [DataMember]
+        public DateTime fecha_registro { set; get; }
+    }
+}
---
+++ SyncPOS/exceptions/CtrlException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace SyncPOS.exceptions
+{
+    public class CtrlException
+    {
+        private static string fileName = string.Format("{0}\\error.log", (object)AppDomain.CurrentDomain.BaseDirectory);
+
+        public static void SetError(string msg)
+        {
+            if (!File.Exists(CtrlException.fileName))
+                File.Create(CtrlException.fileName).Dispose();
+            TextWriter textWriter = (TextWriter)new StreamWriter(CtrlException.fileName, true);
+            textWriter.WriteLine(string.Format("{0} - {1}", (object)DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"), (object)msg));
+            textWriter.Close();
+        }
+
+        public static void SetErrorDB(string Message)
+        {
+            using (POSDataContext posDataContext = new POSDataContext())
+            {
+                /*
+                posDataContext.ctrl_errores.InsertOnSubmit(new ctrl_errores()
+                {
+                    id_error = Guid.NewGuid(),
+                    fecha_log = DateTime.Now,
+                    descripcion = Message
+                });*/
+                posDataContext.SubmitChanges();
+            }
+        }
+
+    }
+}
