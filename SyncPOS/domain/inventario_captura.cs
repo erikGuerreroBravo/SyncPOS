2b8ae20d56e8a8daebb2227f1bc6f2af4d8db519
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-08-27 14:57:58 -0500

    se agrego inventario_captura

---
+++ SyncPOS/domain/inventario_captura.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace SyncPOS.domain
+{
+    [DataContract]
+    public class inventario_captura
+    {
+        [DataMember]
+        public string id_captura { set; get; }
+
+        [DataMember]
+        public string id_inventario_fisico { set; get; }
+
+        [DataMember]
+        public string num_captura { set; get; }
+
+        [DataMember]
+        public string cod_barras { set; get; }
+
+        [DataMember]
+        public string fecha_captura { set; get; }
+
+        [DataMember]
+        public string cant_cja { set; get; }
+
+        [DataMember]
+        public string cant_pza { set; get; }
+    }
+}
