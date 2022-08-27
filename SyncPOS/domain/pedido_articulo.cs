f85cc1088cada1ca3ae91414798526d14cb22af7
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-08-27 15:01:23 -0500

    se agrego la clase pedido_articulo

---
+++ SyncPOS/domain/pedido_articulo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace SyncPOS.domain
+{
+    [DataContract]
+    public class pedido_articulo
+    {
+        [DataMember]
+        public Guid id_pedido { set; get; }
+
+        [DataMember]
+        public short no_articulo { set; get; }
+
+        [DataMember]
+        public string cod_barras { set; get; }
+
+        [DataMember]
+        public string cod_anexo { set; get; }
+
+        [DataMember]
+        public Decimal cantidad { set; get; }
+
+        [DataMember]
+        public Decimal precio_articulo { set; get; }
+
+        [DataMember]
+        public Decimal por_surtir { set; get; }
+
+        [DataMember]
+        public Decimal por_surtir_pzas { set; get; }
+
+        [DataMember]
+        public DateTime fecha_registro { set; get; }
+    }
+}
