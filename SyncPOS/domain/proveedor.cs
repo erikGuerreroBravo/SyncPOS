f12d66b801c627afda4255ddfb114cbc7db63cef
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-08-27 15:02:17 -0500

    se agrego la clase proveedor

---
+++ SyncPOS/domain/proveedor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace SyncPOS.domain
+{
+    [DataContract]
+    public class proveedor
+    {
+        [DataMember]
+        public Guid id_proveedor { set; get; }
+
+        [DataMember]
+        public string rfc { set; get; }
+
+        [DataMember]
+        public string razon_social { set; get; }
+
+        [DataMember]
+        public string estatus { set; get; }
+
+        [DataMember]
+        public DateTime last_change_datetime { set; get; }
+    }
+}
