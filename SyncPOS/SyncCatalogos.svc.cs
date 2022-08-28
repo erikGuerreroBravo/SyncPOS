343c3ebcf3adb329fa2aa40a65aab499303a480e
Jony322 <jonathan_.torres@hotmail.com>
2022-08-28 13:22:39 -0500

    Se hacen modificaciones necesarias

--- SyncPOS/SyncCatalogos.svc.cs
+++ SyncPOS/SyncCatalogos.svc.cs
@@ -1,9 +1,10 @@
-﻿using SyncPOS.domain;
-using SyncPOS.exceptions;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+
+using SyncPOS.DataAccess;
+using SyncPOS.exceptions;
 
 namespace SyncPOS
 {
@@ -13,16 +14,16 @@ namespace SyncPOS
     {
         public List<SyncPOS.domain.unidad_medida> getUnidades(string lastChangeDateTime)
         {
-            using (POSDataContext posDataContext = new POSDataContext())
+            using (PosAdminDataContext PosAdminDataContext = new PosAdminDataContext())
             {
                 if (lastChangeDateTime.Equals((object)DateTime.Parse("01/01/2017 07:00:00")))
-                    return posDataContext.unidad_medida.Select<unidad_medida, SyncPOS.domain.unidad_medida>((Expression<Func<unidad_medida, SyncPOS.domain.unidad_medida>>)(e => new SyncPOS.domain.unidad_medida()
+                    return PosAdminDataContext.unidad_medida.Select<unidad_medida, SyncPOS.domain.unidad_medida>((Expression<Func<unidad_medida, SyncPOS.domain.unidad_medida>>)(e => new SyncPOS.domain.unidad_medida()
                     {
                         id_unidad = e.id_unidad,
                         descripcion = e.descripcion,
                         fecha_registro = DateTime.Now
                     })).ToList<SyncPOS.domain.unidad_medida>();
-                return posDataContext.unidad_medida.Where<unidad_medida>((Expression<Func<unidad_medida, bool>>)(e => e.fecha_registro > DateTime.Parse(lastChangeDateTime))).Select<unidad_medida, SyncPOS.domain.unidad_medida>((Expression<Func<unidad_medida, SyncPOS.domain.unidad_medida>>)(e => new SyncPOS.domain.unidad_medida()
+                return PosAdminDataContext.unidad_medida.Where<unidad_medida>((Expression<Func<unidad_medida, bool>>)(e => e.fecha_registro > DateTime.Parse(lastChangeDateTime))).Select<unidad_medida, SyncPOS.domain.unidad_medida>((Expression<Func<unidad_medida, SyncPOS.domain.unidad_medida>>)(e => new SyncPOS.domain.unidad_medida()
                 {
                     id_unidad = e.id_unidad,
                     descripcion = e.descripcion,
@@ -33,8 +34,8 @@ namespace SyncPOS
 
         public List<SyncPOS.domain.proveedor> getProveedores(string lastChangeDateTime)
         {
-            using (POSDataContext posDataContext = new POSDataContext())
-                return posDataContext.proveedor.Where<proveedor>((Expression<Func<proveedor, bool>>)(a => a.fecha_registro > DateTime.Parse(lastChangeDateTime))).Select<proveedor, SyncPOS.domain.proveedor>((Expression<Func<proveedor, SyncPOS.domain.proveedor>>)(e => new SyncPOS.domain.proveedor()
+            using (PosAdminDataContext PosAdminDataContext = new PosAdminDataContext())
+                return PosAdminDataContext.proveedor.Where<proveedor>((Expression<Func<proveedor, bool>>)(a => a.fecha_registro > DateTime.Parse(lastChangeDateTime))).Select<proveedor, SyncPOS.domain.proveedor>((Expression<Func<proveedor, SyncPOS.domain.proveedor>>)(e => new SyncPOS.domain.proveedor()
                 {
                     id_proveedor = e.id_proveedor,
                     rfc = e.rfc,
@@ -47,8 +48,8 @@ namespace SyncPOS
 
         public List<SyncPOS.domain.articulo> getArticulos(string lastChangeDateTime)
         {
-            using (POSDataContext posDataContext = new POSDataContext())
-                return posDataContext.articulo.Where<articulo>((Expression<Func<articulo, bool>>)(a => a.fecha_registro > DateTime.Parse(lastChangeDateTime) && a.kit.Equals(false))).OrderBy<articulo, DateTime>((Expression<Func<articulo, DateTime>>)(a => a.fecha_registro)).Select<articulo, SyncPOS.domain.articulo>((Expression<Func<articulo, SyncPOS.domain.articulo>>)(a => new SyncPOS.domain.articulo()
+            using (PosAdminDataContext PosAdminDataContext = new PosAdminDataContext())
+                return PosAdminDataContext.articulo.Where<articulo>((Expression<Func<articulo, bool>>)(a => a.fecha_registro > DateTime.Parse(lastChangeDateTime) && a.kit.Equals(false))).OrderBy<articulo, DateTime>((Expression<Func<articulo, DateTime>>)(a => a.fecha_registro)).Select<articulo, SyncPOS.domain.articulo>((Expression<Func<articulo, SyncPOS.domain.articulo>>)(a => new SyncPOS.domain.articulo()
                 {
                     cod_barras = a.cod_barras,
                     cod_asociado = a.cod_asociado,
@@ -78,10 +79,10 @@ namespace SyncPOS
 
         public List<SyncPOS.domain.usuario> getUsuarios(string lastChangeDateTime)
         {
-            using (POSDataContext posDataContext = new POSDataContext())
+            using (PosAdminDataContext PosAdminDataContext = new PosAdminDataContext())
             {
                 if (lastChangeDateTime.Equals((object)DateTime.Parse("01/01/2017 07:00:00")))
-                    return posDataContext.usuario.Select<usuario, SyncPOS.domain.usuario>((Expression<Func<usuario, SyncPOS.domain.usuario>>)(e => new SyncPOS.domain.usuario()
+                    return PosAdminDataContext.usuario.Select<usuario, SyncPOS.domain.usuario>((Expression<Func<usuario, SyncPOS.domain.usuario>>)(e => new SyncPOS.domain.usuario()
                     {
                         user_name = e.user_name,
                         password = e.password,
@@ -89,7 +90,7 @@ namespace SyncPOS
                         enable = e.enable,
                         fecha_registro = DateTime.Now
                     })).ToList<SyncPOS.domain.usuario>();
-                return posDataContext.usuario.Where<usuario>((Expression<Func<usuario, bool>>)(u => u.fecha_registro > DateTime.Parse(lastChangeDateTime))).Select<usuario, SyncPOS.domain.usuario>((Expression<Func<usuario, SyncPOS.domain.usuario>>)(u => new SyncPOS.domain.usuario()
+                return PosAdminDataContext.usuario.Where<usuario>((Expression<Func<usuario, bool>>)(u => u.fecha_registro > DateTime.Parse(lastChangeDateTime))).Select<usuario, SyncPOS.domain.usuario>((Expression<Func<usuario, SyncPOS.domain.usuario>>)(u => new SyncPOS.domain.usuario()
                 {
                     user_name = u.user_name,
                     password = u.password,
@@ -102,17 +103,17 @@ namespace SyncPOS
 
         public List<SyncPOS.domain.usuario_permiso> getPermisosUsuarios(string lastChangeDateTime)
         {
-            using (POSDataContext posDataContext = new POSDataContext())
+            using (PosAdminDataContext PosAdminDataContext = new PosAdminDataContext())
             {
                 if (lastChangeDateTime.Equals((object)DateTime.Parse("01/01/2017 07:00:00")))
-                    return posDataContext.usuario_permiso.Select<usuario_permiso, SyncPOS.domain.usuario_permiso>((Expression<Func<usuario_permiso, SyncPOS.domain.usuario_permiso>>)(u => new SyncPOS.domain.usuario_permiso()
+                    return PosAdminDataContext.usuario_permiso.Select<usuario_permiso, SyncPOS.domain.usuario_permiso>((Expression<Func<usuario_permiso, SyncPOS.domain.usuario_permiso>>)(u => new SyncPOS.domain.usuario_permiso()
                     {
                         user_name = u.user_name,
                         id_permiso = u.id_permiso,
                         valor_num = u.valor_num ?? 0.000M,
                         fecha_registro = u.fecha_registro
                     })).ToList<SyncPOS.domain.usuario_permiso>();
-                return posDataContext.usuario_permiso.Where<usuario_permiso>((Expression<Func<usuario_permiso, bool>>)(up => up.fecha_registro > DateTime.Parse(lastChangeDateTime))).Select<usuario_permiso, SyncPOS.domain.usuario_permiso>((Expression<Func<usuario_permiso, SyncPOS.domain.usuario_permiso>>)(up => new SyncPOS.domain.usuario_permiso()
+                return PosAdminDataContext.usuario_permiso.Where<usuario_permiso>((Expression<Func<usuario_permiso, bool>>)(up => up.fecha_registro > DateTime.Parse(lastChangeDateTime))).Select<usuario_permiso, SyncPOS.domain.usuario_permiso>((Expression<Func<usuario_permiso, SyncPOS.domain.usuario_permiso>>)(up => new SyncPOS.domain.usuario_permiso()
                 {
                     user_name = up.user_name,
                     id_permiso = up.id_permiso,
@@ -122,59 +123,65 @@ namespace SyncPOS
             }
         }
 
-        public List<pedido> GetOrders(string lastChangeDateTime)
+        public List<SyncPOS.domain.pedido> GetOrders(string lastChangeDateTime)
         {
-            using (POSDataContext posDataContext = new POSDataContext())
-                return posDataContext.orden.Where<orden>((Expression<Func<orden, bool>>)(a => a.fecha_registro > DateTime.Parse(lastChangeDateTime))).Select<orden, pedido>((Expression<Func<orden, pedido>>)(a => new pedido()
-                {
-                    id_pedido = a.id_pedido,
-                    num_pedido = a.num_pedido,
-                    fecha_pedido = a.fecha_pedido,
-                    id_proveedor = a.id_proveedor,
-                    status_pedido = a.status_pedido,
-                    fecha_registro = a.fecha_registro
-                })).ToList<pedido>();
+            using (PosAdminDataContext PosAdminDataContext = new PosAdminDataContext())
+                return PosAdminDataContext.orden
+                    .Where<orden>((Expression<Func<orden, bool>>)(a => a.fecha_registro > DateTime.Parse(lastChangeDateTime)))
+                    .Select<orden, SyncPOS.domain.pedido>((Expression<Func<orden, SyncPOS.domain.pedido>>)(a => new SyncPOS.domain.pedido()
+                    {
+                        id_pedido = a.id_pedido,
+                        num_pedido = a.num_pedido,
+                        fecha_pedido = a.fecha_pedido,
+                        id_proveedor = a.id_proveedor,
+                        status_pedido = a.status_pedido,
+                        fecha_registro = a.fecha_registro
+                    })).ToList<SyncPOS.domain.pedido>();
         }
 
-        public List<pedido_articulo> GetOrderDetail(Guid id_pedido)
+        public List<SyncPOS.domain.pedido_articulo> GetOrderDetail(Guid id_pedido)
         {
-            using (POSDataContext posDataContext = new POSDataContext())
-                return posDataContext.orden_articulo.Where<orden_articulo>((Expression<Func<orden_articulo, bool>>)(a => a.id_pedido.Equals(id_pedido))).Select<orden_articulo, pedido_articulo>((Expression<Func<orden_articulo, pedido_articulo>>)(a => new pedido_articulo()
-                {
-                    id_pedido = a.id_pedido,
-                    no_articulo = a.no_articulo,
-                    cod_barras = a.cod_barras,
-                    cod_anexo = a.cod_anexo,
-                    cantidad = a.cantidad,
-                    precio_articulo = a.precio_articulo,
-                    por_surtir = a.por_surtir,
-                    por_surtir_pzas = a.por_surtir_pzas,
-                    fecha_registro = a.fecha_registro
-                })).ToList<pedido_articulo>();
+            using (PosAdminDataContext PosAdminDataContext = new PosAdminDataContext())
+                return PosAdminDataContext.orden_articulo
+                    .Where<orden_articulo>((Expression<Func<orden_articulo, bool>>)(a => a.id_pedido.Equals(id_pedido)))
+                    .Select<orden_articulo, SyncPOS.domain.pedido_articulo>((Expression<Func<orden_articulo, SyncPOS.domain.pedido_articulo>>)(a => new SyncPOS.domain.pedido_articulo()
+                    {
+                        id_pedido = a.id_pedido,
+                        no_articulo = a.no_articulo,
+                        cod_barras = a.cod_barras,
+                        cod_anexo = a.cod_anexo,
+                        cantidad = a.cantidad,
+                        precio_articulo = a.precio_articulo,
+                        por_surtir = a.por_surtir,
+                        por_surtir_pzas = a.por_surtir_pzas,
+                        fecha_registro = a.fecha_registro
+                    })).ToList<SyncPOS.domain.pedido_articulo>();
         }
 
-        public List<pedido_articulo> GetOrderDetailUpdate(string lastChangeDateTime)
+        public List<SyncPOS.domain.pedido_articulo> GetOrderDetailUpdate(string lastChangeDateTime)
         {
-            using (POSDataContext posDataContext = new POSDataContext())
-                return posDataContext.orden_articulo.Where<orden_articulo>((Expression<Func<orden_articulo, bool>>)(a => a.fecha_registro > DateTime.Parse(lastChangeDateTime))).Select<orden_articulo, pedido_articulo>((Expression<Func<orden_articulo, pedido_articulo>>)(a => new pedido_articulo()
-                {
-                    id_pedido = a.id_pedido,
-                    no_articulo = a.no_articulo,
-                    cod_barras = a.cod_barras,
-                    cod_anexo = a.cod_anexo,
-                    cantidad = a.cantidad,
-                    precio_articulo = a.precio_articulo,
-                    por_surtir = a.por_surtir,
-                    por_surtir_pzas = a.por_surtir_pzas,
-                    fecha_registro = a.fecha_registro
-                })).ToList<pedido_articulo>();
+            using (PosAdminDataContext PosAdminDataContext = new PosAdminDataContext())
+                return PosAdminDataContext.orden_articulo
+                    .Where<orden_articulo>((Expression<Func<orden_articulo, bool>>)(a => a.fecha_registro > DateTime.Parse(lastChangeDateTime)))
+                    .Select<orden_articulo, SyncPOS.domain.pedido_articulo>((Expression<Func<orden_articulo, SyncPOS.domain.pedido_articulo>>)(a => new SyncPOS.domain.pedido_articulo()
+                    {
+                        id_pedido = a.id_pedido,
+                        no_articulo = a.no_articulo,
+                        cod_barras = a.cod_barras,
+                        cod_anexo = a.cod_anexo,
+                        cantidad = a.cantidad,
+                        precio_articulo = a.precio_articulo,
+                        por_surtir = a.por_surtir,
+                        por_surtir_pzas = a.por_surtir_pzas,
+                        fecha_registro = a.fecha_registro
+                    })).ToList<SyncPOS.domain.pedido_articulo>();
         }
 
 
         public List<SyncPOS.domain.inventario_fisico> GetInventarios(string lastChangeDateTime)
         {
-            using (POSDataContext posDataContext = new POSDataContext())
-                return posDataContext.inventario_fisico.Where<inventario_fisico>((Expression<Func<inventario_fisico, bool>>)(e => e.fecha_registro > DateTime.Parse(lastChangeDateTime))).Select<inventario_fisico, SyncPOS.domain.inventario_fisico>((Expression<Func<inventario_fisico, SyncPOS.domain.inventario_fisico>>)(e => new SyncPOS.domain.inventario_fisico()
+            using (PosAdminDataContext PosAdminDataContext = new PosAdminDataContext())
+                return PosAdminDataContext.inventario_fisico.Where<inventario_fisico>((Expression<Func<inventario_fisico, bool>>)(e => e.fecha_registro > DateTime.Parse(lastChangeDateTime))).Select<inventario_fisico, SyncPOS.domain.inventario_fisico>((Expression<Func<inventario_fisico, SyncPOS.domain.inventario_fisico>>)(e => new SyncPOS.domain.inventario_fisico()
                 {
                     id_inventario_fisico = e.id_inventario_fisico,
                     id_proveedor = e.id_proveedor,
@@ -189,9 +196,9 @@ namespace SyncPOS
         {
             try
             {
-                using (POSDataContext posDataContext = new POSDataContext())
+                using (PosAdminDataContext PosAdminDataContext = new PosAdminDataContext())
                 {
-                    if (posDataContext.inventario_captura.FirstOrDefault<inventario_captura>((Expression<Func<inventario_captura, bool>>)(e => e.id_inventario_fisico.Equals(new Guid(p.id_inventario_fisico)) && e.num_captura.Equals(long.Parse(p.num_captura)) && e.cod_barras.Equals(p.cod_barras))) == null)
+                    if (PosAdminDataContext.inventario_captura.FirstOrDefault<inventario_captura>((Expression<Func<inventario_captura, bool>>)(e => e.id_inventario_fisico.Equals(new Guid(p.id_inventario_fisico)) && e.num_captura.Equals(long.Parse(p.num_captura)) && e.cod_barras.Equals(p.cod_barras))) == null)
                     {
                         inventario_captura entity = new inventario_captura()
                         {
@@ -203,8 +210,8 @@ namespace SyncPOS
                             cant_cja = Decimal.Parse(p.cant_cja),
                             cant_pza = Decimal.Parse(p.cant_pza)
                         };
-                        posDataContext.inventario_captura.InsertOnSubmit(entity);
-                        posDataContext.SubmitChanges();
+                        PosAdminDataContext.inventario_captura.InsertOnSubmit(entity);
+                        PosAdminDataContext.SubmitChanges();
                     }
                 }
                 return true;
@@ -220,11 +227,11 @@ namespace SyncPOS
         {
             try
             {
-                using (POSDataContext posDataContext = new POSDataContext())
+                using (PosAdminDataContext PosAdminDataContext = new PosAdminDataContext())
                 {
-                    if (posDataContext.compra.FirstOrDefault<compra>((Expression<Func<compra, bool>>)(c => c.id_compra.Equals(p.id_compra))) == null)
+                    if (PosAdminDataContext.compra.FirstOrDefault<compra>((Expression<Func<compra, bool>>)(c => c.id_compra.Equals(p.id_compra))) == null)
                     {
-                        posDataContext.compra.InsertOnSubmit(new compra()
+                        PosAdminDataContext.compra.InsertOnSubmit(new compra()
                         {
                             id_compra = Guid.Parse(p.id_compra),
                             id_proveedor = p.id_proveedor != null ? new Guid?(Guid.Parse(p.id_proveedor)) : new Guid?(),
@@ -236,7 +243,7 @@ namespace SyncPOS
                             no_entrada = (short)0,
                             observaciones = (string)null
                         });
-                        posDataContext.SubmitChanges();
+                        PosAdminDataContext.SubmitChanges();
                     }
                 }
                 return true;
@@ -253,11 +260,11 @@ namespace SyncPOS
         {
             try
             {
-                using (POSDataContext posDataContext = new POSDataContext())
+                using (PosAdminDataContext PosAdminDataContext = new PosAdminDataContext())
                 {
-                    if (posDataContext.compra_articulo.FirstOrDefault<compra_articulo>((Expression<Func<compra_articulo, bool>>)(c => c.id_compra.Equals(p.id_compra) && c.cod_barras.Equals(p.cod_barras) && c.num_articulo.Equals(p.num_articulo))) == null)
+                    if (PosAdminDataContext.compra_articulo.FirstOrDefault<compra_articulo>((Expression<Func<compra_articulo, bool>>)(c => c.id_compra.Equals(p.id_compra) && c.cod_barras.Equals(p.cod_barras) && c.num_articulo.Equals(p.num_articulo))) == null)
                     {
-                        posDataContext.compra_articulo.InsertOnSubmit(new compra_articulo()
+                        PosAdminDataContext.compra_articulo.InsertOnSubmit(new compra_articulo()
                         {
                             id_compra = Guid.Parse(p.id_compra),
                             cod_barras = p.cod_barras,
@@ -268,7 +275,7 @@ namespace SyncPOS
                             no_captura = short.Parse(p.no_captura),
                             no_entrega = Decimal.Parse(p.no_entrega)
                         });
-                        posDataContext.SubmitChanges();
+                        PosAdminDataContext.SubmitChanges();
                     }
                 }
                 return true;
